806be3d27ef9432e59d8615ba16f3d3df45a5450
arnav <arnav.bhati@cloudev.de>
2021-07-13 11:44:01 +0200

    adding elastisearch

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
-from elasticsearch import Elasticsearch
+#from elasticsearch import Elasticsearch
 from redis import Redis
 import rq
 from config import Config
@@ -36,8 +36,8 @@ def create_app(config_class=Config):
     bootstrap.init_app(app)
     moment.init_app(app)
     babel.init_app(app)
-    app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
-        if app.config['ELASTICSEARCH_URL'] else None
+#    app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
+#        if app.config['ELASTICSEARCH_URL'] else None
     app.redis = Redis.from_url(app.config['REDIS_URL'])
     app.task_queue = rq.Queue('microblog-tasks', connection=app.redis)
 
